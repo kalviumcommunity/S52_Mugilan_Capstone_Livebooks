e2ce218c5f3b3a7d87c2ca821aae95688d23cd14
Mugilan Sakthivel <mugilansakthivel@Mugilans-MacBook-Pro.local>
2024-03-13 17:34:08 +0530

    Implement login/logout functionality using access and refresh tokens

---
+++ Server/middlewar/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import ErrorHandler from "./ErrorHandler.js";
+import { CatchAsyncError } from "./catchAsynErrors.js";
+import { redis } from "../utils/redis.js";
+
+
+// authentication user
+export const isAutheticated = CatchAsyncError(async (req, res, next) => {
+    const access_token = req.cookies.access_Token
+
+    if(!access_token){
+        return next(new ErrorHandler('please login to access this resource', 404))
+
+    }
+    const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN)
+    
+    if(!decoded){
+        return next(new ErrorHandler("access token is not valid", 404))
+
+    }
+    const user = await redis.get(decoded.id);
+
+    if(!user){
+        return next(new ErrorHandler("user not found", 400))
+    }
+
+    req.user = JSON.parse(user)
+    next()
+})  
