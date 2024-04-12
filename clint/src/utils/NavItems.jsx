d7faf5efa46271efb54c4fb1b4d5fa1eb0d7a2ec
Mugilan Sakthivel <mugilansakthivel@Mugilans-MacBook-Pro.local>
2024-04-12 17:45:23 +0530

    designed the home page on top and completed the navbar

---
+++ clint/src/utils/NavItems.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const navItemsData = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "AboutUs",
+    url: "/about",
+  },
+  {
+    name: "Courses",
+    url: "/courses",
+  },
+  {
+    name: "Blog",
+    url: "/blog",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+function NavItems({ activeItem, isMobile }) {
+  return (
+    <>
+
+
+      <div className="hidden 800px:flex font-Unbounded font-light text-sm ">
+        {" "}
+        {/* Adjusted class name for responsiveness */}
+        {navItemsData &&
+          navItemsData.map((item, index) => (
+            <Link to={item.url} key={index}>
+              <span
+                className={`${
+                  activeItem === index ? "text-blue-500" : "text-gray-600"
+                }  px-6  `}
+              >
+                {item.name}
+              </span>
+            </Link>
+          ))}
+      </div>
+
+
+      {isMobile && (
+        <div className="800px:hidden mt-5">
+            <div className="w-full text-center py-6">
+                <Link to='/' className=" font-Unbounded text-[24px]">
+                    Hogwarts
+                </Link>
+
+            </div>
+            {navItemsData &&
+              navItemsData.map((i, index) => (
+
+                <Link to={i.url} key={index}>
+                  <span
+                    className={`${
+                      activeItem === index ? "text-blue-500" : "text-gray-600"
+                    }  px-6  block text-[18px] font-Unbounded py-6 `}
+                  >
+                    {i.name}
+                  </span>
+                </Link>
+
+              ))}
+          </div>
+
+      )}
+    </>
+  );
+}
+
+export default NavItems;
---
+++ clint/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        Unbounded : ["Unbounded"]
+      },
+      screens: {
+        '1000px': '1000px',
+        '1100px': '1100px',
+        '1200px': '1200px',
+        '1300px': '1300px',
+        '1500px': '1500px',
+        '1400px': '1400px',
+        '800px': '800px',
+        '400px': '400px',
+      },
+      
+    },
+  },
+  plugins: [],
+}
